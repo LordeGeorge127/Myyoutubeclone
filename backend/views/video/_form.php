28c6b14479dbf8f46c8c7e2d71364d410fd3a7f8
LordeGeorge <george.ken.127@gmail.com>
2023-08-02 20:43:02 +0300

    Nested layouts auth,base,main

---
+++ backend/views/video/_form.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\video $model */
+/** @var yii\bootstrap4\ActiveForm $form */
+?>
+
+<div class="video-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'video_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput() ?>
+
+    <?= $form->field($model, 'tags')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'has_thumbnail')->textInput() ?>
+
+    <?= $form->field($model, 'video_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <?= $form->field($model, 'updated_at')->textInput() ?>
+
+    <?= $form->field($model, 'created_by')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
